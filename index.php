a6ce27b202535dc09352bafc76272c94016a9be4
SangeethanMalliganthan <56502281+SangeethanMalliganthan@users.noreply.github.com>
2019-11-04 15:18:42 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -1,34 +1,32 @@
 <!DOCTYPE html>
 <html>
 	<head>
-	 <body background="back.jpg">
-	<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-    <link href="style.css" rel="stylesheet" id="bootstrap-css">
-    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
-    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+	<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 	<title>Cross Site Request Forgery Protection</title>
+        <style>
+            body{
+                background-image:url("http://www.ecopetit.cat/iconpics/f/1/14365/wallpapers-full-hd-abstract-black.jpg");
+            }
+        </style>
 	</head>
-	<body><br>
-	<h1><center>Synchronizer Token Pattern</center></h1>
+	<body>
+
+
 		<div class="container">
         <div id="login-row" class="row justify-content-center align-items-center">
             <div id="login-column" class="col-md-6">
-                <div class="box">
-                    <div class="shape1"></div>
-                    <div class="shape2"></div>
-                    <div class="shape3"></div>
-                    <div class="shape4"></div>
-                    <div class="shape5"></div>
-                    <div class="shape6"></div>
-                    <div class="shape7"></div>
+                
                     <div class="float">
                         <form class="form" action="result.php" method="post">
+
                             <div class="form-group">
-                                <label for="username" class="text-white">Username:</label><br>
+                                <label for="username" class="text-white"><b>Username : </b></label><br>
                                 <input type="text" name="username" id="username" class="form-control">
                             </div>
                             <div class="form-group">
-                                <label for="password" class="text-white">Password:</label><br>
+                                <label for="password" class="text-white"><b>Password : </b></label><br>
                                 <input type="password" name="password" id="password" class="form-control">
                             </div>
                             <div class="form-group">
@@ -36,7 +34,7 @@
                             </div>
                         </form>
                     </div>
-                </div>
+                
             </div>
         </div>
     </div>
--- result.php
+++ result.php
@@ -4,8 +4,8 @@
 if(isset($_POST['username'],$_POST['password'])){
 	$uname = $_POST['username'];
 	$pwd = $_POST['password'];
-	if($uname == 'admin' && $pwd == 'password321'){
-		echo '<br><h3><center>Successfully logged in</center></h1>';
+	if($uname == 'admin' && $pwd == 'password'){
+		echo '<h3>Successfully logged in</h1>';
 	
 	}
 	else{
@@ -18,13 +18,11 @@ if(isset($_POST['username'],$_POST['password'])){
 
 <!DOCTYPE html>
 <html>
- <body background="back.jpg">
 	<head>
 		<title>Cross Site Request Forgery Protection</title>
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-	<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-    <link href="style.css" rel="stylesheet" id="bootstrap-css">
-    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+	<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 	<script>
 	
 	$(document).ready(function(){
@@ -44,22 +42,19 @@ if(isset($_POST['username'],$_POST['password'])){
 	
 	});
 </script>
+        <style>
+            body{
+                background-image:url("http://www.ecopetit.cat/iconpics/f/1/14365/wallpapers-full-hd-abstract-black.jpg");
+            }
+        </style>
 	</head>
 	<body>
 		<div class="container">
         <div id="login-row" class="row justify-content-center align-items-center">
-            <div id="login-column" class="col-md-6">
-                <div class="box">
-                    <div class="shape1"></div>
-                    <div class="shape2"></div>
-                    <div class="shape3"></div>
-                    <div class="shape4"></div>
-                    <div class="shape5"></div>
-                    <div class="shape6"></div>
-                    <div class="shape7"></div>
+            
                     <div class="float">
                       <form class="form" action="home.php" method="post">
-                            <div class="form-group">
+                            <div class="form-group" align="center">
                                 <label for="username" class="text-white"><h4>Write Something<h4></label><br>
                                 <input type="text" name="updatepost" class="form-control">
                             </div>
@@ -67,11 +62,11 @@ if(isset($_POST['username'],$_POST['password'])){
 					              <input type="hidden" name="token" value="" id="token_to_be_added"/>
 					        </div>
                             <div class="form-group">
-                                <input type="submit"  class="btn btn-info btn-md" value="Update">
+                                <input type="submit"  class="btn btn-info btn-md" value="updatepost">
                             </div>
                       </form>
                     </div>
-                </div>
+                
             </div>
         </div>
     </div>
